58dee66d5adc4deb02a6c9463cc5ff8bd42061e9
rkriar <ramneekriar@gmail.com>
2022-10-02 23:54:41 -0400

    Added divideList; not working, need to tweak FirstHalf

--- question4_merge_sort.pl
+++ question4_merge_sort.pl
@@ -20,7 +20,11 @@ listLength(listTerm(H, T), X) :- listLength(T, M), X is 1 + M.
 
 %%%%% RULE: divideList
 % Add the rule(s) for divideList below
+% divideList(ListTerm, Num, FirstHalf, SecondHalf)
+% FirstHalf contains first Num elements of ListTerm, and SecondHalf contains the remaining.
 
+divideList(listTerm(empty_list, empty_list), 0, listTerm(empty_list, empty_list), listTerm(empty_list, empty_list)).
+divideList(ListTerm, Num, FirstHalf, SecondHalf) :- acc(ListTerm, Num, [], FirstHalf, SecondHalf).
 
 %%%%% RULE: mergeSortedLists
 % Add the rule(s) for mergeSortedLists below
@@ -29,6 +33,10 @@ listLength(listTerm(H, T), X) :- listLength(T, M), X is 1 + M.
 %%%%% Helpers
 % Add any additional helpers below
 
+% divideList Helper
+acc(ListTerm, 0, FirstHalf, FirstHalf, ListTerm).
+acc(listTerm(H, T), Num, FH_listTerm, FirstHalf, SecondHalf) :- Num > 1, New_Num is (Num - 1), acc(T, New_Num, listTerm(FH_listTerm, H), FirstHalf, SecondHalf). 
+acc(listTerm(H, T), Num, FH_listTerm, FirstHalf, SecondHalf) :- Num = 1, New_Num is (Num - 1), acc(T, New_Num, listTerm(FH_listTerm, listTerm(H, empty_list)), FirstHalf, SecondHalf). 
 
 %%%%% RULE: mergeSort
 % Add the rule(s) for mergeSort below
